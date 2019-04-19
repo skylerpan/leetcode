49c71c8b63f57432fb652b73030f3b155cb8797f
skylerpan <skylerpan@gmail.com>
2019-04-20 00:33:05 +0800

    [Advanced] Solve it without "strlen".

---
+++ 0014.Longest_Common_Prefix/README.md
@@ -0,0 +1,1 @@
+1. Advanced: solve it without "strlen".
--- 0014.Longest_Common_Prefix/solution.c
+++ 0014.Longest_Common_Prefix/solution.c
@@ -3,15 +3,12 @@
 #include <string.h>
 
 char* longestCommonPrefix(char** strs, int strsSize) {
-    int i, j, first_str_len;
+    int i = 0, j;
 
-    first_str_len = strsSize == 0 ? 
-        0 : strlen(strs[0]);
-        
-    if(first_str_len == 0)
+    if(strsSize == 0 || strs[0][0] == 0)
         return "";
 
-    for(i = 0; i < first_str_len; i++) {
+    do {
         for(j = 0; j < strsSize; j++) {
             if(strs[0][i] != strs[j][i])
             {
@@ -19,7 +16,8 @@ char* longestCommonPrefix(char** strs, int strsSize) {
                 return strs[0];
             }
         }
-    }
+    } while(strs[0][++i] !='\0');
+
 
     return strs[0];
 }
