a114b947ec0561d30c797a50f6cb89174f15191d
skylerpan <skylerpan@gmail.com>
2019-05-28 22:55:36 +0800

    Solve leetcode 0031. Reverse version.

--- 0031.Next_Permutation/README.md
+++ 0031.Next_Permutation/README.md
@@ -1,4 +1,4 @@
-Thinking:
+## Thinking:
 1. Observe the sequence [3, 2, 1, 4, 3]. The next permutation is [3, 2, 3, 1, 4].
 2. We can find out the changed numbers which appear at last few elements.
 3. So, We can check the permutation from end.
@@ -7,10 +7,13 @@ Thinking:
 6. Because we want to find the next permutation, sorting the last n-1 numbers can get the next one.
 7. When check to the first element and no larger number after it, just sorting all the sequence.
 
-Example:
+## Example:
 * iteration 1: [3]
 * iteration 2: [4, 3]
 * iteration 3: [1, 4, 3] -> 3 and 4 are larger than 1. Change 1 and 3. -> [3, 4, 1].
 * Sort sequence after 3 -> [3, 1, 4].
 * Find the next permutation [3, 2, 3, 1, 4].
 
+## Reverse version:
+* Same as almost sorting's steps but choice the closet one with last n when we has more than one number that larger than last n and small than others.
+* Use "reverse" to replace "sort".
--- 0031.Next_Permutation/solution.cpp
+++ 0031.Next_Permutation/solution.cpp
@@ -1,5 +1,30 @@
 #include <math>
 using namespace std;
+#if 1 // reverse version
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        for(int i = nums.size()-2; i > -1; i--) {
+            int next_greater = i;
+            for(int j = i+1; j < nums.size(); j++)
+            {
+                if( nums[j] > nums[i] ) {
+                    if(next_greater == i)
+                        next_greater = j;
+                    else
+                        next_greater = nums[j] <= nums[next_greater] ? j : next_greater;
+                }
+            }
+            if(next_greater == i)
+                continue;
+            swap(nums[i], nums[next_greater]);
+            reverse(nums.begin()+i+1, nums.end());
+            return;
+        }
+        reverse(nums.begin(), nums.end());
+    }
+};
+#else // sorting version
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
@@ -22,3 +47,4 @@ public:
         sort(nums.begin(), nums.end());
     }
 };
+#endif
